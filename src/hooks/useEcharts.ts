1e4ca1ee2ec70313e105dbb53f316bf778320543
yueyueyan <yueyueyan1215@163.com>
2025-12-18 00:37:32 +0800

    finish all

--- src/hooks/useEcharts.ts
+++ src/hooks/useEcharts.ts
@@ -1,5 +1,38 @@
-import * as echarts from "echarts";
 import { onMounted, onUnmounted, ref, watch, type Ref } from "vue";
+
+import * as echarts from "echarts/core";
+import { BarChart, PieChart, RadarChart, LineChart } from "echarts/charts";
+// 引入标题，提示框，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent,
+  ToolboxComponent,
+} from "echarts/components";
+// 标签自动布局、全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from "echarts/features";
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from "echarts/renderers";
+
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  BarChart,
+  PieChart,
+  RadarChart,
+  LineChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+  LegendComponent,
+  ToolboxComponent,
+]);
 
 export const useEcharts = (options: Ref<echarts.EChartsCoreOption>) => {
   const chartRef = ref<HTMLDivElement | null>(null);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import viteCompression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    viteCompression({
+      deleteOriginFile: false,
+      threshold: 10240, //体积大于10k才进行压缩
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+    viteCompression({
+      deleteOriginFile: false,
+      threshold: 10240, //体积大于10k才进行压缩
+      algorithm: "brotliCompress",
+      ext: ".br",
+    }),
+    visualizer({ open: true }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          echarts: ["echarts"],
+          vue: ["vue"],
+          dataV: ["@kjgl77/datav-vue3"],
+        },
+      },
+    },
+  },
+});
